944f542c8dbd84d36d29203b28e90f45390d7c8e
smoric <smoric@poczta.onet.pl>
2020-11-02 18:09:38 +0100

    fixy

--- gcp/modules/network/main.tf
+++ gcp/modules/network/main.tf
@@ -11,20 +11,17 @@ provider "google" {
   region  = var.region
 }
 
-# VPC NET
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
   auto_create_subnetworks = "false"
 }
 
-# VPC REGION
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
-
 
 resource "google_compute_firewall" "vpc" {
   name    = "vpc-firewall"
