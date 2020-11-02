276980f1953849a06138dbb0b13fb768e337f353
smoric <smoric@poczta.onet.pl>
2020-11-02 18:02:43 +0100

    add jenkins host

--- gcp/modules/compute_engines/main.tf
+++ gcp/modules/compute_engines/main.tf
@@ -32,3 +32,23 @@ resource "google_compute_instance" "provisioning-master" {
 
   metadata_startup_script = file("${path.module}/scripts/ansimble-install.sh")
 }
+
+resource "google_compute_instance" "jenkins-server" {
+  name         = "jenkins-server"
+  machine_type = "n1-standard-1"
+  zone         = var.zone
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-xenial-v20201014"
+    }
+  }
+
+  network_interface {
+    network = var.vpc_name
+    subnetwork  = var.vpc_subnet_name
+    access_config {
+    }
+  }
+}
