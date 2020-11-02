6662a4f1584e33cc243faa8c87e064d992e74c72
smoric <smoric@poczta.onet.pl>
2020-11-02 17:57:16 +0100

    ansible silent

--- gcp/modules/compute_engines/scripts/ansimble-install.sh
+++ gcp/modules/compute_engines/scripts/ansimble-install.sh
@@ -1,4 +1,4 @@
 sudo apt update
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt --assume-yes install software-properties-common
+sudo apt-add-repository --assume-yes --update ppa:ansible/ansible
+sudo apt --assume-yes install ansible
